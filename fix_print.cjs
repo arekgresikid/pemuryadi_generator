ad1f71d6419583996959a3506ab42a25ade56bf3
Pemuryadi <arekgresikid@gmail.com>
2026-06-16 05:55:49 +0700

    chore: remove temporary nodejs scripts

--- fix_print.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/utils/print.ts', 'utf8');
-code = code.replace(/\\`/g, '`');
-code = code.replace(/\\\$/g, '$');
-fs.writeFileSync('src/utils/print.ts', code);
-console.log('Fixed print.ts');
--- test_regex.cjs
+++
@@ -1,2 +0,0 @@
-const t = '##Ringkasan Materi Ajar###Tujuan Pembelajaran';
-console.log(t.replace(/(#{1,6})\s*/g, '\n\n$1 '));
