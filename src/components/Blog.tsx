445b28e5b402916da9827ad0cebe6ac184ea0a6b
Pemuryadi <arekgresikid@gmail.com>
2026-06-24 04:52:35 +0700

    style: reduce top padding in blog page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -26,7 +26,7 @@ export default function Blog() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50/50 pt-24 pb-12 px-4 sm:px-6 lg:px-8">
+    <div className="w-full bg-gray-50/50 pt-6 pb-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-4xl mx-auto space-y-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
         
         <div className="text-center space-y-4">
